6e956b66197f7c7de8f8b0f72b11fd8dfaa7d8f9
KartikChawla-droid <70290203+KartikChawla-droid@users.noreply.github.com>
2024-04-15 16:22:27 +0200

    remove theme settings sidebar, move dark/light mode toggle to settings page, change default theme to dark

--- src/layout/composables/layout.js
+++ src/layout/composables/layout.js
@@ -2,10 +2,10 @@ import { toRefs, reactive, computed } from 'vue';
 
 const layoutConfig = reactive({
     ripple: true,
-    darkTheme: false,
+    darkTheme: true,
     inputStyle: 'outlined',
     menuMode: 'static',
-    theme: 'aura-light-green',
+    theme: 'aura-dark-green',
     scale: 14,
     activeMenuItem: null
 });
