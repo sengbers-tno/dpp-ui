c8bdd4dc0346766ac4bc7a0218c02e827dba64f0
Sharon <sharon.engbers@tno.nl>
2024-04-16 17:06:14 +0200

    Adjust link to profile page

--- src/components/ProfileButton.vue
+++ src/components/ProfileButton.vue
@@ -1,11 +1,53 @@
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const menu = ref();
 const items = ref([
-    { id: 1, name: 'Alice', role: 'Admin' },
-    { id: 2, name: 'Bob', role: 'Viewer' },
-    { id: 3, name: 'Liam', role: 'Editor' }
+    {
+        label: 'Users',
+        items: [
+            {
+                name: 'Alice',
+                icon: 'pi pi-user',
+                role: 'Admin'
+            },
+            {
+                name: 'Bob',
+                icon: 'pi pi-user',
+                role: 'Viewer'
+            },
+            {
+                name: 'Liam',
+                icon: 'pi pi-user',
+                role: 'Editor'
+            }
+        ]
+    },
+    {
+        separator: true
+    },
+    {
+        label: 'Profile',
+        items: [
+            {
+                name: 'Settings',
+                icon: 'pi pi-cog',
+                command: () => {
+                    router.push('/profile');
+                }
+            },
+            {
+                name: 'Logout',
+                icon: 'pi pi-sign-out',
+                command: () => {
+                    router.push('/');
+                }
+            }
+        ]
+    }
 ]);
 
 const toggle = (event) => {
@@ -14,9 +56,10 @@ const toggle = (event) => {
 
 const selectedItem = ref(null);
 const handleMenuItemClick = (item) => {
-    console.log(item.role);
-    selectedItem.value = item;
-    console.log(`Selected item: ${selectedItem.value.name}`);
+    console.log(item);
+    if (item.role) {
+        selectedItem.value = item;
+    }
 };
 </script>
 
@@ -26,17 +69,20 @@ const handleMenuItemClick = (item) => {
             <i class="pi pi-user"></i>
             <span class="inline-flex flex-column pl-3 pt-2">
                 <span v-if="selectedItem" class="font-bold">{{ selectedItem.name }}</span>
-                <span v-else class="font-bold">{{ items[0].name }}</span>
+                <span v-else class="font-bold">{{ items[0].items[0].name }}</span>
                 <span v-if="selectedItem" class="text-sm">{{ selectedItem.role }}</span>
-                <span v-else class="text-sm">{{ items[0].role }}</span>
+                <span v-else class="text-sm">{{ items[0].items[0].role }}</span>
             </span>
         </button>
         <Menu ref="menu" id="dropdown-menu" :model="items" :popup="true">
+            <template #submenuheader="{ item }">
+                <span class="text-primary font-bold">{{ item.label }}</span>
+            </template>
             <template #item="{ item, props }">
                 <a v-ripple :key="item.id" @click="handleMenuItemClick(item)" v-bind="props.action">
-                    <i class="pi pi-user"></i>
+                    <i :class="item.icon"></i>
                     <span class="ml-2">{{ item.name }}</span>
-                    <span class="ml-2">({{ item.role }})</span>
+                    <span v-if="item.role" class="ml-2">({{ item.role }})</span>
                 </a>
             </template>
         </Menu>
