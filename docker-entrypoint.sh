38800d57489e215f34c162912dfc766a190a801c
abhishekmaha23 <abhishek.mahadevan23@gmail.com>
2024-06-27 13:45:11 +0200

    Fix trailing slash on env variable problem

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+# Remove trailing slash if present from API_ENDPOINT
+API_ENDPOINT=$(echo "${API_ENDPOINT}" | sed 's#/$##')
 envsubst '${API_ENDPOINT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
